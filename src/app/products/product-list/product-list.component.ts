b15326122462be6dbadaaf6bdfe61b42dbf2e804
Filip Tijan <ftijan@gmail.com>
2021-12-30 21:24:27 +0000

    input, output data passing

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -62,4 +62,8 @@ export class ProductListComponent implements OnInit {
     return this.products.filter((product: Product) => 
       product.productName.toLocaleLowerCase().includes(filterBy));
   }
+
+  onRatingClicked(message: string) {
+    this.pageTitle = 'Product List ' + message;
+  }
 }
