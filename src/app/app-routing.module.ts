d93e5a8ab557b3bd7026576d7356714f027750b9
Filip Tijan <ftijan@gmail.com>
2022-01-01 21:23:51 +0000

    module separation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomeComponent } from './home/welcome/welcome.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
 
-const routes: Routes = [
-  { path: 'products', component: ProductListComponent },
+const routes: Routes = [ 
   { 
-    path: 'products/:id', 
-    canActivate: [ ProductDetailGuard ],
-    component: ProductDetailComponent,
+    path: 'products',
+    loadChildren: () => import('./products/product.module').then(m => m.ProductModule)
   },
   { path: 'welcome', component: WelcomeComponent },
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,22 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star/star.component';
 import { WelcomeComponent } from './home/welcome/welcome.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    WelcomeComponent,
-    ProductDetailComponent,    
+    WelcomeComponent,    
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule,    
     HttpClientModule,
     AppRoutingModule,
+    ProductModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
