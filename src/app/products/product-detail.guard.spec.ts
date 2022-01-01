cf067d3f2c7759f60fef2b6f913a56a3954d6934
Filip Tijan <ftijan@gmail.com>
2022-01-01 22:10:16 +0000

    fixing unit tests

--- src/app/products/product-detail.guard.spec.ts
+++ src/app/products/product-detail.guard.spec.ts
@@ -1,4 +1,6 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ProductDetailGuard } from './product-detail.guard';
 
@@ -6,7 +8,12 @@ describe('ProductDetailGuard', () => {
   let guard: ProductDetailGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+    });
     guard = TestBed.inject(ProductDetailGuard);
   });
 
